9c5f15b5b3376afefdc66d6c7cd442e6c25e144b
DouglasBruce <douglasbruce96@gmail.com>
2023-11-30 23:41:04 +0000

    Add new route for web sockets

---
+++ src/main/kotlin/com/douglasbruce/data/models/ChatMessage.kt
@@ -0,0 +1,10 @@
+package com.douglasbruce.data.models
+
+import com.douglasbruce.utils.Constants.TYPE_CHAT_MESSAGE
+
+data class ChatMessage(
+    val from: String,
+    val roomName: String,
+    val message: String,
+    val timestamp: Long,
+) : BaseModel(TYPE_CHAT_MESSAGE)
