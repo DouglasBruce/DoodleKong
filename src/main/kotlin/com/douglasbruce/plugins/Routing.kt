92a64554c184a56c9acf76de21f6f13c8f862918
Douglas Bruce <douglasbruce96@gmail.com>
2023-11-30 23:03:56 +0000

    Merge pull request (#5) from join-room

--- src/main/kotlin/com/douglasbruce/plugins/Routing.kt
+++ src/main/kotlin/com/douglasbruce/plugins/Routing.kt
@@ -2,6 +2,7 @@ package com.douglasbruce.plugins
 
 import com.douglasbruce.routes.createRoomRoute
 import com.douglasbruce.routes.getRoomsRoute
+import com.douglasbruce.routes.joinRoomRoute
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
@@ -9,5 +10,6 @@ fun Application.configureRouting() {
     install(Routing) {
         createRoomRoute()
         getRoomsRoute()
+        joinRoomRoute()
     }
 }
