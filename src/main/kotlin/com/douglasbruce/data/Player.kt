c9d54ec6fb8f850cfd7e5f124892438ea8d6469a
Douglas Bruce <douglasbruce96@gmail.com>
2023-11-30 17:15:57 +0000

    Merge pull request (#2) from create-base-server-structure

---
+++ src/main/kotlin/com/douglasbruce/data/Player.kt
@@ -0,0 +1,12 @@
+package com.douglasbruce.data
+
+import io.ktor.websocket.*
+
+data class Player(
+    val username: String,
+    val socket: WebSocketSession,
+    val clientId: String,
+    var isDrawing: Boolean,
+    var score: Int = 0,
+    var rank: Int = 0,
+)
